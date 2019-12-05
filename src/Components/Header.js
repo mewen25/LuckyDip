3097fd44a35e3c60fbaf10c44e81e916a14e5a7e
Mewen <mewen25@gmail.com>
2019-12-05 12:59:47 +0000

    inital_creation

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Searchbar from "./Searchbar";
 
 function Header() {
   return (
     <div className="header">
       <h1>LuckyDip LTD</h1>
+      <Searchbar />
     </div>
   );
 }
